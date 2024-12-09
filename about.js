723feee5070edb92d79e9164017b12115867f202
BugHunter-X <askzenai@gmail.com>
2024-12-09 05:58:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,33 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import {useEffect, useState} from 'react';
+
+export default function About() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('useEffect called');
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div>About Page - Count: {count}</div>
+  );
+}
+```
